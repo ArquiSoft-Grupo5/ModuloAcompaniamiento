6dd29c97fe35662edca6a02b54f525cf3efe9076
juanprograma <69698438+juanprograma@users.noreply.github.com>
2022-05-14 03:33:31 -0500

    si

--- Educacion/monitoring/urls.py
+++ Educacion/monitoring/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path 
 from . import views
-from django.contrib.auth.views import login
+"""from django.contrib.auth.views import login"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('', include('citas.urls')),
     path('', include('acompaniantes.urls')),
-    path (r'^$', login,{'template_name': 'index.html'}, name='login')  
+    """path (r'^$', login,{'template_name': 'index.html'}, name='login') """ 
 ]
 
